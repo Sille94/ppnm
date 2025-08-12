1071a917b8f466925ac477b7dc6abe45e0cbd0b5
Cecilie <cecilie.kb.madsen@gmail.com>
2025-08-12 11:36:18 +0200

    montecarlo 6points

--- homework/montecarlo/main.cs
+++ homework/montecarlo/main.cs
@@ -6,11 +6,22 @@ public static class main{
 	public static void Main(){
 		//integration of unitcircle  in cartesian coordinates \int_{-1}^{1}\int_{-1}^{1} x^2+y^2 dydx
 		Func<vector, double> f= x=>{return Sqrt(1-x[1]*x[1]);};
+		var outstrm = new System.IO.StreamWriter("unitcircle.txt");
 		for(int N=10000; N<=1000000; N+=10000){
 			vector a = new vector("1,1");
 			vector b = new vector("-1,-1");
 			(double val, double err) = monte.plainmc(f,a,b,N);
-			WriteLine($"{N},{err},{Abs(PI-val)},{1/Sqrt(N)}");
+			outstrm.WriteLine($"{N},{err},{Abs(PI-val)},{1/Sqrt(N)}");
 			}
+		outstrm.Close();
+		//calculating (1/pi)^3\int_{0}^{pi}\int_{0}^{pi}\int_{0}^{pi}[1-cos(x)cos(y)cos(z)]dxdydz
+		Func<vector, double> g = x=>{return 1/PI*1/PI*1/PI/(1-Cos(x[0])*Cos(x[1])*Cos(x[2]));};
+		vector a2 = new vector("0,0,0");
+		vector b2 = new vector(3);
+		for(int i=0; i<3; i++){b2[i]=PI;}
+		int N2 = 100000;
+		(double val2, double err2) = monte.plainmc(g,a2,b2,N2);
+		WriteLine($"val = {val2} +/- {err2}");
+		WriteLine($"1.3932039296856768591842462603255");
 		}
 }
