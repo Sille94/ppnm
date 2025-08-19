4979c0455923bf5f623b1794a1272764793b3812
Cecilie <cecilie.kb.madsen@gmail.com>
2025-08-19 20:29:23 +0200

    minimization 9 points

--- homework/minimization/main.cs
+++ homework/minimization/main.cs
@@ -8,21 +8,22 @@ public static class main{
 		WriteLine("Rosenbrock's valley function");
 		Func<vector, double> rv = x =>{return Pow(1-x[0],2)+100*Pow(x[1]-Pow(x[0],2),2);};
 		vector x0_rv = new vector("100,100");
-		(vector x_rv, int i) = mini.newton(rv,x0_rv);
-		if(i<10001){
+		(vector x_rv, int count1) = mini.newton(rv,x0_rv);
+		if(count1<10001){
 			WriteLine($"minimum found at rv({x_rv[0]},{x_rv[1]}), expected at (1,1)");
-			WriteLine($"with {i} iterations");
+			WriteLine($"with {count1} iterations");
 			}
 		WriteLine("");
 		WriteLine("Himmelblau's function");
 		Func<vector,double> hb = x =>{return Pow(Pow(x[0],2)+x[1]-11,2)+Pow(x[0]+Pow(x[1],2)-7,2);};
 		vector x0_hb = new vector("50,50");
-		(vector x_hb, int j) = mini.newton(hb,x0_hb);
-			if(i<10001){
+		(vector x_hb, int count2) = mini.newton(hb,x0_hb);
+			if(count2<10001){
 				WriteLine($"minimum found at hb({x_hb[0]},{x_hb[1]}), expectet at (3,2), (-2.8,3.13), (-3.78,-3.28) or (3.58,-1.85)");
-				WriteLine($"with {j} iterations");
+				WriteLine($"with {count2} iterations");
 				}
 		WriteLine("Part B");
+		//loading data from file
 		var energy = new genlist<double>();
 		var signal = new genlist<double>();
 		var error = new genlist<double>();
@@ -31,10 +32,50 @@ public static class main{
 		do{
 			string line=Console.In.ReadLine();
 			if(line==null)break;
-			string[] words = line.Split(seperators,options);
-			energy.add(double.Parse(Words[0]));
-			signal.add(double.Parse(Words[1]));
-			error.add(double.Parse(Words[2]));
+			string[] words = line.Split(separators,options);
+			energy.add(double.Parse(words[0]));
+			signal.add(double.Parse(words[1]));
+			error.add(double.Parse(words[2]));
 			}while(true);
+		Func<vector, double> bw = p =>{//The Breit-Winger function
+			double E = p[0];//energy
+			double m = p[1];//mass
+			double G = p[2];//width
+			double A = p[3];//amplitude
+			return A/(Pow(E-m,2)+Pow(G,2)/4);
+			};
+		Func<vector, double> deviation = p =>{//The Deviation function
+			double m=p[0];
+			double G=p[1];
+			double A=p[2];
+			double sum=0;
+			for(int i=0; i<energy.size; i++){
+				vector paramE = new vector(energy[i],m,G,A);
+				sum+=Pow((bw(paramE)-signal[i])/error[i],2);
+				}
+			return sum;
+			};
+		double m_guess = 125.9;
+		double g_guess = 7;
+		double A_guess = 60;
+		vector init = new vector(m_guess,g_guess,A_guess);
+		WriteLine($"Minimizing with initial guess m={m_guess}, Gamma={g_guess}, A={A_guess}");
+		(vector fit_param, int count) =mini.newton(deviation,init);
+		WriteLine($"Found with {count} itarations, parameters for Higgs are m={fit_param[0]}, Gamma = {fit_param[1]}, A={fit_param[2]}");
+		var outstrm = new System.IO.StreamWriter("higgs_fit.txt");
+		//creating fit
+		int n = 100;
+		double step =(double)(159-101)/(double)(n-1);
+		double fit_m = fit_param[0];
+		double fit_G = fit_param[1];
+		double fit_A = fit_param[2];
+		vector es = new vector(n);
+		for(int i=0; i<n; i++){es[i]=100+i*step;}
+		for(int i=0; i<n; i++){
+			vector param = new vector(es[i],fit_m,fit_G,fit_A);
+			//vector param = new vector(es[i],125,7,66);
+			outstrm.WriteLine($"{es[i]},{bw(param)}");
+			}	
+		outstrm.Close();
 		}
 }
--- homework/minimization/mini.cs
+++ homework/minimization/mini.cs
@@ -3,48 +3,52 @@ using static System.Console;
 using static System.Math;
 
 public static class mini{
-	public static (vector,int) newton(Func<vector,double> phi, vector x, double acc=1e-3){
+	public static (vector,int) newton(Func<vector,double> f, vector x, double acc=1e-3){
 		int i=0;
 		do{
-			if(i>10000){WriteLine("Max iterations reached"); break;}
-			vector g = gradient(phi,x);
-			if(g.norm() < acc) break;
-			matrix H = hessian(phi,x);
+			if(i>1000){WriteLine("Max iterations reached"); break;}
+			vector gf = gradient(f,x);
+			if(gf.norm() < acc) break;
+			matrix H = hessian(f,x);
 			(matrix Q, matrix R) = QR.decomp(H);
-			vector dx = QR.solve(Q,R,-g);
-			double l = 1;
+			vector dx = QR.solve(Q,R,-gf);
+			double l = 1.0;
 			do{
-				if(phi(x+l*dx)<phi(x)) break;
-				if(1/1024 <= l) break;
-				l /= 2;
+				if(f(x+l*dx)<f(x)) break;
+				if(1.0/1024 > l) break;
+				l=l/2;
 				}while(true);
 			x+=l*dx;
 			i++;
 			}while(true);
 		return (x,i);	
 		}
-	static vector gradient(Func<vector,double> phi, vector x){
-		double phix = phi(x);
-		vector gphi = new vector(x.size);
+	static vector gradient(Func<vector,double> f, vector x){
+		double fx = f(x);
+		vector gf = new vector(x.size);
 		for(int i=0 ;i<x.size; i++){
 			double dxi = (1+Abs(x[i]))*Pow(2,-26);
-			vector x_step = x.copy();
-			x_step[i]+=dxi;
-			gphi[i]=(phi(x_step)-phix)/dxi;
+			//vector x_step = x.copy();
+			//x_step[i]+=dxi;
+			x[i]+=dxi;
+			gf[i]=(f(x)-fx)/dxi;
+			x[i]-=dxi;
 			}
-		return gphi;
+		return gf;
 		}
-	static matrix hessian(Func<vector,double> phi,vector x){
+	static matrix hessian(Func<vector,double> f,vector x){
 		matrix H = new matrix(x.size,x.size);
-		vector gphix = gradient(phi,x);
+		vector gfx = gradient(f,x);
 		for(int j=0; j<x.size; j++){
 			double dxj = (1+Abs(x[j]))*Pow(2,-13);
-			vector step = x.copy();
-			step[j] +=dxj;
-			vector dgphi = gradient(phi,step)-gphix;
+			//vector x_step = x.copy();
+			//x_step[j] +=dxj;
+			x[j]+=dxj;
+			vector dg = gradient(f,x)-gfx;
 			for(int i=0; i<x.size;i++){
-				H[i,j]=dgphi[i]/dxj;
+				H[i,j]=dg[i]/dxj;
 				}
+			x[j]-=dxj;
 			}
 		return H;
 		}
