a7aacafb35db360b4c76922fe09bf153a8bf2158
Cecilie <cecilie.kb.madsen@gmail.com>
2025-08-20 11:49:01 +0200

    Integration 9 points

--- homework/integration/integration.cs
+++ homework/integration/integration.cs
@@ -3,25 +3,40 @@ using static System.Math;
 using static System.Console;
 
 public static class integ{
-	public static double integrate(Func<double, double> f, double a, double b, double acc,double eps, double f2=Double.NaN, double f3=Double.NaN){
+	public static double integrate(Func<double,double>f,double a,double b,double acc, double eps, ref int count, double f2=Double.NaN, double f3=Double.NaN){
+		if(!double.IsInfinity(a)&&!double.IsInfinity(b)){return integrator(f,a,b,acc,eps,ref count);}
+		if(double.IsNegativeInfinity(a)&&double.IsPositiveInfinity(b)){Func<double,double> u = t=>(f((1-t)/t) + f(-(1-t)/t))/Pow(t,2);return integrator(u,0,1,acc,eps,ref count);}
+		if(!double.IsInfinity(a)&&double.IsPositiveInfinity(b)){Func<double,double> u = t =>f(a+(1-t)/t)*t/Pow(1-t,2);return integrator(u,0,1,acc,eps,ref count);}
+		if(double.IsNegativeInfinity(a)&& !double.IsInfinity(b)){Func<double,double> u = t=>f(b-(1-t)/t)/Pow(t,2); return integrator(u,0,1,acc,eps,ref count);}
+		else{throw new Exception("Wrong Integration limit");}
+		}
+	static double integrator(Func<double, double> f, double a, double b, double acc,double eps, ref int count, double f2=Double.NaN, double f3=Double.NaN){
+		count++;
 		double h = b-a;
 		if(Double.IsNaN(f2)){f2=f(a+2*h/6);f3=f(a+4*h/6);}
 		double f1=f(a+h/6), f4=f(a+5*h/6);
-		double Q = (2*f1+f2+f3+2*f4)/6*(b-a);
-		double q = (f1+f2+f3+f4)/4*(b-a);
+		double Q = (2*f1+f2+f3+2*f4)/6*h;
+		double q = (f1+f2+f3+f4)/4*h;
 		double err = Abs(Q-q);
 		if(err <= acc+eps*Abs(Q)){return Q;}
-		else {return integrate(f,a,(a+b)/2,acc/Sqrt(2),eps,f1,f2)+integrate(f,(a+b)/2,b,acc/Sqrt(2),eps,f3,f4);}
+		else {return integrate(f,a,(a+b)/2,acc/Sqrt(2),eps,ref count, f1,f2)+integrate(f,(a+b)/2,b,acc/Sqrt(2),eps,ref count,f3,f4);}
 		}
+
 	public static double erf(double z, double acc, double eps){
+		int count=0;
 		if(0<=z&&z<=1){
 			Func<double,double>f=x=>{double y=Exp(-(x*x));return y;};
-			return 2/Sqrt(PI)*integrate(f,0,z,acc,eps);
+			return 2/Sqrt(PI)*integrate(f,0,z,acc,eps,ref count);
 			}
 		if(1<=z){
 			Func<double,double>f=t=>{double y=Exp(-(z+(1-t)/t)*(z+(1-t)/t)/t/t);return y;};
-			return 1-2/Sqrt(PI)*integrate(f,0,1,acc,eps);
+			return 1-2/Sqrt(PI)*integrate(f,0,1,acc,eps,ref count);
 			}
 		else{return -erf(-z,acc,eps);}
+		}
+
+	public static double clenshaw_curtis(Func<double,double>f, double a,double b,double acc,double eps,ref int count){
+		Func<double,double> cc = thet => f((a+b)/2+(b-a)/2*Cos(thet))*Sin(thet)*(b-a)/2;
+		return integrate(cc,0,PI,acc,eps,ref count);
 		}
 }
--- homework/integration/main.cs
+++ homework/integration/main.cs
@@ -6,22 +6,45 @@ static class main{
 	static public void Main(){
 		double acc = 0.001;
 		double eps = 0.001;
+		WriteLine("Part A");
+		WriteLine("Debug using some functions");
+		WriteLine("");
+
 		Func<double,double>f1 = x =>{double y=Sqrt(x);return y;};
-		double val1 = integ.integrate(f1,0,1,acc,eps);
-		WriteLine($"val1={Round(val1,3)}");		
-		WriteLine($"2/3={Round((double)2/(double)3,3)}");
+		int count1=0;
+		double val1 = integ.integrate(f1,0,1,acc,eps,ref count1);
+		WriteLine($"func1={val1}");		
+		WriteLine($"expected: 2/3={(double)2/(double)3}");
+		WriteLine($"no of iterations: {count1}");
+		WriteLine($"");
+
 		Func<double,double>f2 = x =>{double y=1/Sqrt(x);return y;};
-		double val2 = integ.integrate(f2,0,1,acc,eps);
-		WriteLine($"val2={Round(val2,3)}");
-		WriteLine($"{Round((double)2,3)}");
+		int count2=0;
+		double val2 = integ.integrate(f2,0,1,acc,eps,ref count2);
+		WriteLine($"func2={val2}");
+		WriteLine($"expected: 2");
+		WriteLine($"no of interations: {count2}");
+		WriteLine("");
+
 		Func<double,double>f3 = x =>{double y=Sqrt(1-x*x);return y;};
-		double val3 = integ.integrate(f3,0,1,acc,eps);
-		WriteLine($"val3={Round(val3,3)}");
-		WriteLine($"Pi/4={Round(PI/4,3)}");
+		int count3=0;
+		double val3 = integ.integrate(f3,0,1,acc,eps,ref count3);
+		WriteLine($"func3={val3}");
+		WriteLine($"expected: Pi/4={PI/(double)4}");
+		WriteLine($"no of interations: {count3}");
+		WriteLine("");
+		
 		Func<double,double>f4 = x =>{double y=Log(x)/Sqrt(x);return y;};
-		double val4 = integ.integrate(f4,0,1,acc,eps);
-		WriteLine($"val4={Round(val4,3)}");
-		WriteLine($"{-4}");
+		int count4 =0;
+		double val4 = integ.integrate(f4,0,1,acc,eps,ref count4);
+		WriteLine($"func4={val4}");
+		WriteLine($"expected: -4");
+		WriteLine($"no of iterations: {count4}");
+		WriteLine("");
+		
+
+		WriteLine("Error Function can be viewed in errorFunc.svg");
+		WriteLine("");
 		var outstrm1 = new System.IO.StreamWriter("erf_data.txt");
 		var outstrm2 = new System.IO.StreamWriter("erf_eps.txt");
 		for(double i=0; i<0.1; i+=0.02){
@@ -36,11 +59,30 @@ static class main{
 		double boterf1 = 0.84270079294971486934;
 		for(double a=0.1; a>1/1E5; a=a/10){
 			var erf1 = integ.erf(1,a,0);
-			WriteLine($"{boterf1}");
-			WriteLine($"{erf1},{Abs(boterf1-erf1)}");
 			outstrm2.WriteLine($"{a},{Abs(boterf1-erf1)}");
 			}
 		outstrm1.Close();
 		outstrm2.Close();
+		
+		WriteLine("Part B");
+		WriteLine("");
+		Func<double,double> g = x => 1/Sqrt(x);
+		Func<double,double> h = x => Log(x)/Sqrt(x);
+		int iter_ccg=0,iter_cch=0,iter_ig=0,iter_ih=0;
+		double ccg = integ.clenshaw_curtis(g,0,1,acc,eps,ref iter_ccg);
+		double cch = integ.clenshaw_curtis(h,0,1,acc,eps,ref iter_cch);
+		double ig = integ.integrate(g,0,1,acc,eps,ref iter_ig);
+		double ih = integ.integrate(h,0,1,acc,eps,ref iter_ih);
+		WriteLine("1/Sqrt(x):");
+		WriteLine($"Integration with Clenshaw-Curtis yielded {ccg} (expected 2) with {iter_ccg} iterations - compared to {iter_ig} iterations with the original integration routine");
+		WriteLine("");
+		WriteLine("Log(x)/Sqrt(x):");
+		WriteLine($"Integration with Clenshaw-Curtis yielded {cch} (expected -4) with {iter_cch} iterations - compared to {iter_ih} iterations with the original integration routine");	
+		WriteLine("");
+		WriteLine("testing a converging infinite limit integral: Sin(x)/x from 0 to -inf ");
+		Func<double,double> gen = x => Sin(x)/x;
+		int count = 0;
+		double igen = integ.integrate(gen,0,double.PositiveInfinity,acc,eps,ref count);
+		WriteLine($"Expected value pI/2 = {PI/2} - Integration yielded {igen}, with {count} iterations");
 		}
 }
